ac4941263ed7ded97f7a5e78ad8d52b94bf9a818
Karine Paiva <karineppaiva24@gmail.com>
2026-02-04 17:13:02 -0300

    feat: projeto React criado com Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="public\logo.png" alt="" />
+      </header>
+      <section>
+        <img src="public/banner.png" alt="" />
+      </section>
+    </main>
+  )
+}
+
+export default App
